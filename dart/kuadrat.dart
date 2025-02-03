7f5b8ae73d7767a443ca7fb758686a68c423e52a
Labibluthfi11 <muhammadlabibluthfitrpl@gmail.com>
2025-02-03 16:53:15 +0700

    Update kuadrat.dart

--- dart/kuadrat.dart
+++ dart/kuadrat.dart
@@ -1,11 +1,21 @@
-List<int> squareList(List<int> numbers) {
-  return numbers.map((num) => num * num).toList();
-}
+// File: kuadrat.dart
 
 void main() {
-  List<int> originalList = [1, 2, 3, 4, 5];
-  List<int> squaredList = squareList(originalList);
+  // Contoh 1: Menghitung kuadrat dari sebuah bilangan
+  int angka = 7;
+  int hasil = kuadrat(angka);
+  print("Kuadrat dari $angka adalah $hasil");
 
-  print("List Asli: $originalList");
-  print("List Kuadrat: $squaredList");
+  // Contoh 2: Menghitung kuadrat dari setiap elemen dalam sebuah list
+  List<int> deretAngka = [1, 2, 3, 4, 5];
+  // Menggunakan fungsi map untuk menerapkan fungsi kuadrat ke setiap elemen list
+  List<int> hasilKuadrat = deretAngka.map(kuadrat).toList();
+  print("Deret angka asli: $deretAngka");
+  print("Deret kuadrat: $hasilKuadrat");
+}
+
+/// Fungsi untuk menghitung kuadrat dari suatu bilangan.
+/// Misalnya, kuadrat(3) akan menghasilkan 9.
+int kuadrat(int x) {
+  return x * x;
 }
